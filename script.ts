b71faa82b0b53cd379eb3c7424ed9f89332a61b0
Leo Zhao <lz527@cornell.edu>
2022-09-19 22:54:47 -0400

    add line 2

--- script.ts
+++ script.ts
@@ -1,2 +1,3 @@
 var display = 'This is Ar545 first website!';
-document.body.innerHTML = `<div>${display}</div>`
+var happy = 'I am happy to be here!';
+document.body.innerHTML = `<div><h1>${display}</h1><div>${happy}</div></div>`
